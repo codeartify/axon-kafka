9740291feb63a0a2155a38e35e0bc84b127ec692
Oliver Zihler <oliver.zihler@raiffeisen.ch>
2025-12-13 12:36:51 +0100

    adapt config to axon doc.

--- customer-service/src/main/kotlin/com/codeartify/customerservice/KafkaEventConsumptionConfig.kt
+++ customer-service/src/main/kotlin/com/codeartify/customerservice/KafkaEventConsumptionConfig.kt
@@ -1,7 +1,9 @@
 package com.codeartify.customerservice
 
 import org.axonframework.config.EventProcessingConfigurer
+import org.axonframework.extensions.kafka.eventhandling.consumer.AsyncFetcher
 import org.axonframework.extensions.kafka.eventhandling.consumer.ConsumerFactory
+import org.axonframework.extensions.kafka.eventhandling.consumer.DefaultConsumerFactory
 import org.axonframework.extensions.kafka.eventhandling.consumer.Fetcher
 import org.axonframework.extensions.kafka.eventhandling.consumer.subscribable.SubscribableKafkaMessageSource
 import org.axonframework.serialization.Serializer
@@ -10,8 +12,28 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.stereotype.Component
 
+
 @Configuration
-class EventProcessorConfig {
+class KafkaEventConsumptionConfig {
+
+    @Bean
+    fun consumerFactory(): ConsumerFactory<String, ByteArray> {
+        val consumerConfiguration = mapOf<String, Any>(
+            "bootstrap.servers" to "localhost:9092",
+            "key.deserializer" to "org.apache.kafka.common.serialization.StringDeserializer",
+            "value.deserializer" to "org.apache.kafka.common.serialization.ByteArrayDeserializer",
+            "enable.auto.commit" to "true",
+            "auto.commit.interval.ms" to "3000"
+        )
+        return DefaultConsumerFactory(consumerConfiguration)
+    }
+
+    @Bean
+    fun fetcher(): Fetcher<*, *, *> {
+        return AsyncFetcher.builder<String, ByteArray, org.axonframework.eventhandling.EventMessage<*>>()
+            .pollTimeout(5000)
+            .build()
+    }
 
     @Bean
     fun subscribableKafkaMessageSource(
